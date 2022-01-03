577bb7c213332d45c2278ab7eb52ddb03d3effa6
Michael Sanguinete <michael.sanguinete@gmail.com>
2022-01-03 16:42:52 -0300

    Adicionando as classes Entidade

---
+++ src/main/java/com/compass/finalproject/entity/StatusDenuncia.java
@@ -0,0 +1,7 @@
+package com.compass.finalproject.entity;
+
+public enum StatusDenuncia {
+	
+	Tratado, Em_Tratativa, Nao_Tratado
+
+}
---
+++ src/main/java/com/compass/finalproject/entity/Usuario.java
@@ -0,0 +1,28 @@
+package com.compass.finalproject.entity;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String nome;
+	private Endereco endereco;
+	private String telefone;
+	private String email;
+	private String senha;
+	private List<Denuncias> listaDenuncias;
+
+}
