3e91d6d87b8f44310438edd55ec0ea3254b51a79
Michael Sanguinete <michael.sanguinete@gmail.com>
2022-01-06 12:02:59 -0300

    Adicionando arquivo para popular o banco de dados

--- src/main/java/com/compass/finalproject/entity/OrgaoResponsavel.java
+++ src/main/java/com/compass/finalproject/entity/OrgaoResponsavel.java
@@ -8,6 +8,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -15,6 +16,7 @@ import lombok.Setter;
 @Setter
 @Getter
 @Entity
+@Table(name = "ORGAORESPONSAVEL")
 public class OrgaoResponsavel {
 
 	@Id
