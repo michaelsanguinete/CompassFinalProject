0f6bba6bd62deb2cd37340437d32af3348b1222e
Michael Sanguinete <michael.sanguinete@gmail.com>
2022-01-12 18:29:02 -0300

    Alterando formato da exibição da DataDaCriação

--- src/main/java/com/compass/finalproject/DTO/DenunciaDTO.java
+++ src/main/java/com/compass/finalproject/DTO/DenunciaDTO.java
@@ -1,6 +1,8 @@
 package com.compass.finalproject.DTO;
 
 import com.compass.finalproject.entity.*;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,5 +20,6 @@ public class DenunciaDTO {
     private Animais animal;
     private Endereco enderecoDenuncia;
     private StatusDenuncia status;
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss", shape = JsonFormat.Shape.STRING)
     private LocalDateTime dataCriacao;
 }
--- src/main/java/com/compass/finalproject/DTO/DenunciaFormDTO.java
+++ src/main/java/com/compass/finalproject/DTO/DenunciaFormDTO.java
@@ -1,6 +1,8 @@
 package com.compass.finalproject.DTO;
 
 import com.compass.finalproject.entity.*;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,5 +19,6 @@ public class DenunciaFormDTO {
     private Animais tipoAnimal;
     private Endereco enderecoDenuncia;
     private StatusDenuncia status;
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss", shape = JsonFormat.Shape.STRING)
     private LocalDateTime dataCriacao;
 }
