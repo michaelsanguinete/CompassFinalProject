3a97538d0b2ac5b5a6f0dede17e469ae83d7a0b1
eduardo-braz <eduardobrazc@gmail.com>
2022-01-13 15:23:15 -0300

    Busca denuncias atreladas a determinado usuario

--- src/main/java/com/compass/finalproject/controller/UsuarioController.java
+++ src/main/java/com/compass/finalproject/controller/UsuarioController.java
@@ -2,16 +2,16 @@ package com.compass.finalproject.controller;
 
 import javax.transaction.Transactional;
 
+import com.compass.finalproject.DTO.DetalhesDenunciaDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.compass.finalproject.DTO.UsuarioDTO;
 import com.compass.finalproject.DTO.UsuarioFormDTO;
 import com.compass.finalproject.service.UsuarioService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/usuario")
@@ -26,6 +26,10 @@ public class UsuarioController {
 		return this.usuarioService.save(body);
 	}
 	
-	
+	@GetMapping("/{id}/denuncias")
+	@Transactional
+	public ResponseEntity<List<DetalhesDenunciaDTO>> denunciasDoUsuario(@PathVariable int id){
+		return this.usuarioService.denunciasDoUsuario(id);
+	}
 
 }
