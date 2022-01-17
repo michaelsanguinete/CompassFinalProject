45a4511b17649bb054b0ec313b600fab18135411
Breno Veloso <bbreno.valadares@gmail.com>
2022-01-17 14:59:05 -0300

    Validacoes entidades Usuario, endereco e OrgaoResponsavel

--- src/main/java/com/compass/finalproject/entity/Endereco.java
+++ src/main/java/com/compass/finalproject/entity/Endereco.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
@@ -16,12 +17,18 @@ public class Endereco {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	@NotNull
 	private String logradouro;
+	@NotNull
 	private String estado;
 	private String complemento;
+	@NotNull
 	private String bairro;
+	@NotNull
 	private String cidade;
+	@NotNull
 	private int numero;
+	@NotNull
 	private int cep;
 
 }
--- src/main/java/com/compass/finalproject/entity/OrgaoResponsavel.java
+++ src/main/java/com/compass/finalproject/entity/OrgaoResponsavel.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
 import java.util.List;
 
 @Setter
@@ -15,13 +17,18 @@ public class OrgaoResponsavel {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	@NotNull
 	private String nome;
 	@OneToOne
+	@NotNull
 	private Endereco endereco;
+	@NotNull
 	private String telefone;
 	@OneToMany
 	private List<Denuncias> listaDenuncias;
+	@NotNull
 	private String email;
+	@NotNull
 	private String senha;
 
 }
