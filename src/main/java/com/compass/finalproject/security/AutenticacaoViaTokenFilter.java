ddc86c7d8a85f5ceaf82ac8dde303cde503b6993
Michael Sanguinete <michael.sanguinete@gmail.com>
2022-01-18 22:14:15 -0300

    Alterações finais Security

--- src/main/java/com/compass/finalproject/security/AutenticacaoViaTokenFilter.java
+++ src/main/java/com/compass/finalproject/security/AutenticacaoViaTokenFilter.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.compass.finalproject.entity.Usuario;
